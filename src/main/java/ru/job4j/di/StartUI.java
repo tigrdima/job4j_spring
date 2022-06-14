e4b831b529beef2ab349c58561bfd4544168686c
tigrdima <duys79yandex.ru>
2022-06-14 13:39:06 +0300

    0. Что такое DI? [#6876]

---
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -0,0 +1,25 @@
+package ru.job4j.di;
+
+public class StartUI {
+    private final Store store;
+   private final ConsoleInput consoleInput;
+
+    public StartUI(Store store, ConsoleInput consoleInput) {
+        this.store = store;
+        this.consoleInput = consoleInput;
+    }
+
+  public String input() {
+      return consoleInput.askStr("Input: ");
+    }
+
+    public void add() {
+        store.add(input());
+    }
+
+    public void print() {
+        for (String value : store.getAll()) {
+            System.out.println(value);
+        }
+    }
+}
